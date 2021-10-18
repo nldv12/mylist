168cde518d394e4078e40dc23d7755fc047922fa
Nazar Majewski <nldv12@gmail.com>
2021-10-18 15:42:09 +0200

    wersja prosta

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="myTitle">Baśnie braci Grimm
+        </h1>
+        <div className="myList">
+          <div className="myListElement">1. Żabi Król lub Żelazo Henryk</div>
+          <div className="myListElement">2. Spółka kota z myszą</div>
+          <div className="myListElement">3. Dziecko Matki Bożej</div>
+          <div className="myListElement">4. Bajka o tym, co wyruszył, by nauczyć się bać</div>
+          <div className="myListElement">5. O wilku i siedmiu koźlątkach</div>
+          <div className="myListElement">6. Wierny Jan</div>
+          <div className="myListElement">7. Dobry interes</div>
+          <div className="myListElement">8. O dziwnym grajku</div>
+        </div>
       </header>
     </div>
   );
